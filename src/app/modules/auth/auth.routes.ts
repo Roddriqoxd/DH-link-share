858a729f8a1f47287c74d3ba264d17fb800b48ec
roddriqoxd@gmail.com <rodrigo>
2025-09-26 23:24:30 -0400

    feat: setup initial router-first architecture with lazy loaded feature routes

---
+++ src/app/modules/auth/auth.routes.ts
@@ -0,0 +1,16 @@
+import {Routes} from '@angular/router';
+
+export const AUTH_ROUTES: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.component')
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register.component')
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  }
+];
