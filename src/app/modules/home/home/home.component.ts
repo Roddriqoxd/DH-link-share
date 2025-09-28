55301a78e23d34e5c777dfcb9c2d7574b625daca
roddriqoxd@gmail.com <rodrigo>
2025-09-27 21:38:33 -0400

    feat: add links page for home component

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -1,8 +1,15 @@
 import {Component} from '@angular/core';
+import {StartedMessageComponent} from '../../../shared/components/started-message/started-message.component';
+import {PreviewPhoneComponent} from '../../../shared/components/preview-phone/preview-phone.component';
+import {LinkOptionComponent} from '../../../shared/components/link-option/link-option.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    StartedMessageComponent,
+    PreviewPhoneComponent,
+    LinkOptionComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
