466c558a41d219ac9c79f0bea9e9ea2b466762b7
roddriqoxd@gmail.com <rodrigo>
2025-09-28 00:06:29 -0400

    refactor: change the way content is rendered within the home component

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -2,13 +2,17 @@ import {Component} from '@angular/core';
 import {StartedMessageComponent} from '../../../shared/components/started-message/started-message.component';
 import {PreviewPhoneComponent} from '../../../shared/components/preview-phone/preview-phone.component';
 import {LinkOptionComponent} from '../../../shared/components/link-option/link-option.component';
+import {AddedLinksComponent} from '../added-links/added-links.component';
+import {ProfileDetailsComponent} from '../profile-details/profile-details.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     StartedMessageComponent,
     PreviewPhoneComponent,
-    LinkOptionComponent
+    LinkOptionComponent,
+    AddedLinksComponent,
+    ProfileDetailsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
